537a2568ddf322e1891cb71996656da96977bb98
joeyoungsang <127814297+joeyoungsang@users.noreply.github.com>
2023-11-16 17:46:17 +0900

    회원가입/유져 페이지 재구현
    회원가입/유져 페이지 재구현,
    모달 만들기

--- src/pages/User/User/User.jsx
+++ src/pages/User/User/User.jsx
@@ -1,184 +1,32 @@
 import styled from 'styled-components';
 import { useRef, useState } from 'react';
-import DeleteModal from './DeleteModal';
-import AddressChangeModal from './AddressChangeModal';
-import TelChangeModal from './TelChangeModal';
-import PasswordChangeModal from './PasswordChangeModal';
+import AddressChangeModal from '../AddressChangeModal/AddressChangeModal';
+import DeleteModal from '../DeleteModal/DeleteModal';
+import TelChangeModal from '../TelChangeModal/TelChangeModal';
+import PasswordChangeModal from '../PasswordModal/PasswordChangeModal';
+import {
+	AccountDiv,
+	DeleteYourAccountDiv,
+	GenderDiv,
+	ModalDiv,
+	ProductRegisterButton,
+	ProfileImg,
+	ProfileImgDiv,
+	ProfileImgInput,
+	ProfileImgLabel,
+	SignOutDiv,
+	UserBox,
+	UserContent,
+	UserItemContentDiv,
+	UserItemDiv,
+	UserItemTitleDiv,
+	UserLeftItemDiv,
+	UserNamePhotoDiv,
+	UserPhotoDiv,
+	UserRightItemDiv,
+	UserWrapper,
+} from './User.style';
 
-const UserWrapper = styled.body`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	width: 100%;
-	height: 100vh;
-	flex-direction: column;
-`;
-
-const UserBox = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	width: 1380px;
-	height: 100vh;
-	flex-direction: column;
-`;
-
-const AccountDiv = styled.div`
-	width: 90%;
-	height: 40px;
-	border-bottom: 2px solid #434343;
-	font-weight: 500;
-	font-size: 30px;
-	padding-bottom: 50px;
-	position: absolute;
-	top: 80px;
-`;
-
-const UserContent = styled.div`
-	padding-top: 30px;
-	width: 90%;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-`;
-
-const UserNamePhotoDiv = styled.div`
-	display: flex;
-	position: absolute;
-	top: 160px;
-`;
-
-const UserPhotoDiv = styled.div`
-	width: 100px;
-	height: 100px;
-`;
-
-const UserNameDiv = styled.div`
-	font-size: 30px;
-	font-weight: 500;
-	color: #434343;
-	height: 63px;
-	width: 200px;
-	display: flex;
-	align-items: center;
-	justify-content: left;
-	padding-left: 20px;
-`;
-
-const UserItemDiv = styled.div`
-	flex-direction: column;
-	padding-top: 200px;
-`;
-
-const UserLeftItemDiv = styled.div`
-	width: 782px;
-	height: 48px;
-	border-bottom: 1px solid #434343;
-	font-size: 20px;
-	justify-content: space-between;
-	display: flex;
-	align-items: center;
-	margin-top: 16px;
-	padding-right: 8px;
-`;
-
-const UserRightItemDiv = styled.div`
-	width: 427px;
-	height: 48px;
-	border-bottom: 1px solid #434343;
-	font-size: 20px;
-	justify-content: space-between;
-	display: flex;
-	align-items: center;
-	margin-top: 16px;
-	padding-right: 8px;
-`;
-
-const UserItemTitleDiv = styled.div`
-	width: 200px;
-	height: 48px;
-	display: flex;
-	align-items: center;
-`;
-
-const UserItemContentDiv = styled.div`
-	width: 500px;
-	height: 48px;
-	display: flex;
-	align-items: center;
-	justify-content: left;
-`;
-
-const ProductRegisterButton = styled.button`
-	width: 427px;
-	height: 64px;
-	background-color: #5d2510;
-	font-size: 20px;
-	color: #ede1d2;
-	border: 0;
-	&:hover {
-		cursor: pointer;
-	}
-`;
-
-const SignOutDiv = styled.div`
-	width: 64px;
-	padding-top: 100px;
-	border-bottom: 1px solid #434343;
-	font-size: 18px;
-	&:hover {
-		cursor: pointer;
-	}
-`;
-const DeleteYourAccountDiv = styled.div`
-	padding-top: 20px;
-	border-bottom: 1px solid #434343;
-	font-size: 18px;
-	&:hover {
-		cursor: pointer;
-	}
-`;
-const ProfileImg = styled.img`
-	width: 100%;
-	height: 100%;
-	object-fit: cover;
-`;
-const ProfileImgDiv = styled.div`
-	width: 100px;
-	height: 100px;
-	border-radius: 50px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	background-color: transparent;
-	overflow: hidden;
-	border: 2px solid var(--color-main-text);
-`;
-
-const ProfileImgLabel = styled.label`
-	margin: 5px 0 20px 0;
-	font-size: 16px;
-	color: var(--color-main-text);
-	display: inline-block;
-	cursor: pointer;
-`;
-
-const ProfileImgInput = styled.input`
-	display: none;
-`;
-
-const ModalDiv = styled.div`
-	&:hover {
-		cursor: pointer;
-	}
-`;
-const GenderDiv = styled.div`
-	width: 60px;
-	height: 30px;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-`;
 const User = () => {
 	const [imgFile, setImgFile] = useState('');
 	const imgRef = useRef();
