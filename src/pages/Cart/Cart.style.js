019c4dba24325024310f7b1c0655ad02d133e7e8
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-16 22:16:09 +0900

    feat: 장바구니 아이템 개별 선택 및 전체 선택 기능 구현

--- src/pages/Cart/Cart.style.js
+++ src/pages/Cart/Cart.style.js
@@ -13,9 +13,28 @@ export const CartTitleH2 = styled.h3`
 	font-weight: 500;
 `;
 export const CartContentsDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 `;
 export const CartItemsDiv = styled.div`
-  width: 65%;
-`
+	width: 65%;
+`;
+export const CartItemCheckInput = styled.input`
+	display: none;
+	& + label {
+		display: inline-block;
+		content: '';
+		width: 18px;
+		height: 18px;
+		margin-right: 8px;
+		border-radius: 50%;
+		border: 1px solid #434343;
+		cursor: pointer;
+	}
+	&:checked + label {
+		background-color: #434343;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+`;
