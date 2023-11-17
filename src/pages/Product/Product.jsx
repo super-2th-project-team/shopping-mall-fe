d4f58292eeefbfab32733c7b0509431b23ceb7d0
DataCodeLiteracy <dataliteracy@icloud.com>
2023-11-17 12:08:11 +0900

    feat: product 페이지에서 scroll up, down 구현
    2023.11.17

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,10 +1,12 @@
 import ProductDetail from '../../components/product/ProductDetail/ProductDetail';
 import { ProductSection } from '../../components/product/ProductDetail/ProductDetail.style';
+import ScrollButton from '../../components/scroll/ScrollButton/ScrollButton';
 
 const Product = () => {
 	return (
 		<ProductSection>
 			<ProductDetail />
+			<ScrollButton />
 		</ProductSection>
 	);
 };
