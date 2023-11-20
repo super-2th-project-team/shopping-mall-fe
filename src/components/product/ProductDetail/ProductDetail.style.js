5873c4d3bb98a1c634de8f0e63736ac87a612caa
joeyoungsang <127814297+joeyoungsang@users.noreply.github.com>
2023-11-20 16:15:50 +0900

    Revert "로그인/회원가입/회원정보 페이지 api 연결"

--- src/components/product/ProductDetail/ProductDetail.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const ProductSection = styled.section`
-	width: 70%;
-	min-width: 1320px;
-	margin: auto;
-	margin-top: 120px;
-	margin-bottom: 80px;
-`;
